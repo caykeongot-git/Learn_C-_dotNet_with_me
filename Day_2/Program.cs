923b051d61c7cf7891d9c0bc8d6fa67dc7357b8b
An Ninh <ninhza2005@gmail.com>
2025-11-21 11:34:36 +0700

    Second push ahihihihihihihihihih

---
+++ Day_2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data.SqlTypes;
+
+namespace Day_2_ENCAPSULATION
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+
+            SINHVIEN temp = new SINHVIEN();
+            SINHVIEN sv = new SINHVIEN("2380601602", "Nguyen Dinh An Ninh", 3.31);
+            sv.display();
+            
+        }
+
+    }
+}
---
+++ Day_2/SINHVIEN.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_2_ENCAPSULATION
+{
+    internal class SINHVIEN
+    {
+        private string _maSinhVien;
+        private string _hoTen;
+        private double _gpa;
+        private static int countStudent { get; set; } = 0;
+        public string HoTen { get; set; }
+        public string MaSinhVien { 
+            get { return _maSinhVien; }
+            set { _maSinhVien = value; }
+        }
+        public double GPA
+        {
+            get { return _gpa; }
+            set
+            {
+                if (_gpa >= 0 && _gpa <= 4.0) _gpa = value;
+                else _gpa = 0;
+            }
+        }
+
+        public SINHVIEN ()
+        {
+            Console.WriteLine("Ham tao khong co tham so");
+            _maSinhVien = "NULL";
+            _hoTen = "NULL";
+            _gpa = 0;   
+        }
+
+        public SINHVIEN (string maSinhVien, string hoTen , double gpa)
+        {
+            this._maSinhVien = maSinhVien;
+            this._gpa = gpa;
+            this._hoTen = hoTen;
+            ++countStudent;
+        }
+
+        public void display ()
+        {
+            Console.WriteLine($"{_maSinhVien} - {_hoTen} - {_gpa}");
+        }
+
+        public static void ThongTinTruong ()
+        {
+            Console.WriteLine("Lam gi co bien thong tin truong");
+        }
+
+
+    }
+}
